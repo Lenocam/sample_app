708bf40ed15706b43388fbfdf5d0a80dc09f7c45
Reed MacConnell <Learn22Ruby@gmail.com>
2015-11-13 12:12:45 -0500

    Resetting the password

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,7 +62,12 @@ validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
 		#Send password reset email.
 		def send_password_reset_email
-			UserMailer.password_reset(self).deliver_now			
+			UserMailer.password_reset(self).deliver_now
+		end
+
+		#Returns true if a password reset has expired.
+		def password_reset_expired?
+			reset_sent_at < 2.hours.ago
 		end
 
 		private
